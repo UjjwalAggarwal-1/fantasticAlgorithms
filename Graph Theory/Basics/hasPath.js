340a72be716d0131667557c691b3e9dbf35d4f88
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2022-09-28 23:33:46 +0530

    has Path

--- Graph Theory/Basics/hasPath.js
+++ Graph Theory/Basics/hasPath.js
@@ -1,7 +1,7 @@
 /*
     "graph" aka "adjency-list"
 */
-graph = {
+graph = { //directed path
     'a':['c','b'],
     'b':['h'],
     'c':['d'],
@@ -73,3 +73,78 @@ const BFS=(graph,src)=>{
 // };
 
 // BFSr(graph,'a');
+
+
+function hasPath(graph,src,dst) {
+    if (src===dst) return true;
+
+    for (neighbour of graph[src]){
+        if (hasPath(graph,neighbour,dst)===true){
+            return true;
+        }
+        // else{
+        //     continue;
+        // }
+    }
+
+    return false;
+};
+
+// console.log(hasPath(graph,'c','g'));
+
+
+
+edges = [
+    ['i','j'],
+    ['i','k'],
+    ['k','m'],
+    ['k','l'],
+    ['o','n'],
+]
+
+function E2G (edges){
+    Wörterbuch = {}
+    for(a of edges){
+        for(halfa of a){
+            if (!(halfa in Wörterbuch)) Wörterbuch[halfa] = [];
+        };
+    };
+    for(a of edges){
+        Wörterbuch[a[0]].push(a[1]);
+        Wörterbuch[a[1]].push(a[0]);
+    };
+    return Wörterbuch;
+};
+//need this thingy below
+undGraph = {
+    'i':['j','k'],
+    'j':['i'],
+    'k':['i','m','l'],
+    'm':['k'],
+    'l':['k'],
+    'o':['n'],
+    'n':['o'],
+};
+
+function undPath(edges,nodeA,nodeB){
+    const graph = E2G(edges);
+    return hasPathMOD(graph, nodeA,nodeB,new Set());
+};
+function hasPathMOD(graph,src,dst,vis) {
+    if (src===dst) return true;
+    if (!(src in vis)) vis.add(src);
+    for (neighbour of graph[src]){
+        // half an hour of BT for using neighbour in vis rather than vis.has(neighbour)
+        if (!(vis.has(neighbour))){
+            vis.add(neighbour);
+            console.log(vis)
+            if (hasPathMOD(graph,neighbour,dst,vis)===true){
+                return true;
+            };
+        };
+    };
+
+    return false;
+};
+
+// console.log(undPath(edges,'i','o'));
