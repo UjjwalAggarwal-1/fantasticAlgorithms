17bf1569ff500cd3b294f9272e907af2ad8b9d58
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2023-03-04 09:33:16 +0530

    modified:   DSA/labsheets/DSA/ujju/lab1,2/linked_list/linked_list.c

--- DSA/labsheets/DSA/ujju/lab1,2/linked_list/linked_list.c
+++ DSA/labsheets/DSA/ujju/lab1,2/linked_list/linked_list.c
@@ -13,7 +13,52 @@ struct linked_list{
     NODE head;
 };
 
+LIST createNewList(){
+    LIST l = (LIST) malloc(sizeof(struct linked_list));
+    l->count = 0;
+    l->head = (NODE) malloc(0*sizeof(struct node));
+    return l;
+}
 
+NODE createNewNode(int value){
+    NODE n = (NODE) malloc(sizeof(struct node));
+    n->next = (NODE) malloc(0*sizeof(struct node));
+    n->ele = value;
+    return n;
+}
+
+void insertAfter(int ele, NODE n, LIST l){
+
+    if (l->head == NULL) {
+        l->head = n;
+        l->count++;
+        n->next = NULL;
+        return;
+    };
+
+    NODE now = l->head;
+    int fnd = 0;
+    while (now){
+        if (now->ele = ele){
+            n->next = now->next;
+            now->next = n;
+            l->count++;
+            fnd = 1;
+            break;
+        }
+        now = now->next;
+    };
+
+    if (!fnd){
+        while (now->next){
+          now = now->next;
+        };
+        l->count++;
+        now->next = n;
+        n->next = NULL;
+    }
+    return;
+}
 
 int main(){
 
